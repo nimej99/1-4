0ad55730eb1da62cf48f803e5576d125a6d56639
nicee1009 <je1009@hanmail.net>
2023-03-07 21:31:09 +0900

    sub

--- script/sub.js
+++ script/sub.js
@@ -25,33 +25,70 @@ $(function(){
     let sPos = $(this).scrollTop();
     console.log(sPos);
 
+    //첫화면 텍스트 효과 스크롤값
+    if(sPos>=960){
+      $("#main_intro_txt, main_intro_txt02").hide();
+    }else{
+      $("#main_intro_txt").fadeIn(1000);
+      $("#main_intro_txt02").fadeIn(2000);
+    }
+
+    //숫자 올라가게
     if(sPos>=937){
-      $(".intro").fadeIn(3000);
-      $('.count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 2000, // 2초 동안 애니메이션 효과 적용
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-      });
+      // $(".intro").fadeIn(3000);
+      // $('.count').each(function () {
+      //   $(this).prop('Counter',0).animate({
+      //       Counter: $(this).text()
+      //   }, {
+      //       duration: 500, // 2초 동안 애니메이션 효과 적용
+      //       easing: 'swing',
+      //       step: function (now) {
+      //           $(this).text(Math.ceil(now));
+      //       }
+      //   });
+      // });
     }else{
     }
+
+
+    if(sPos>=1500){
+      $('.mission_box').addClass('left_on');
+      $('.vision_box').addClass('right_on');
+    }else{
+      $('.mission_box').removeClass('left_on');
+      $('.vision_box').removeClass('right_on');
+    }
+
   });
   
 
 });
 
-const missionBox = document.querySelector('.mission_box');
 
-window.addEventListener('scroll', () => {
-  const scrollPosition = window.scrollY + window.innerHeight;
-  const missionBoxPosition = missionBox.offsetTop + missionBox.offsetHeight / 2;
+
+
+// const missionBox = document.querySelector('.mission_box');
+
+// window.addEventListener('scroll', () => {
+//   const scrollPosition = window.scrollY + window.innerHeight;
+//   const missionBoxPosition = missionBox.offsetTop + missionBox.offsetHeight / 2;
   
-  if (scrollPosition > missionBoxPosition) {
-    missionBox.classList.add('show');
-  }
-});
+//   if (scrollPosition > missionBoxPosition) {
+//     missionBox.classList.add('show');
+//   }
+// });
+
+
+
+
+// 왼쪽박스,  오른쪽 박스에 적용할 서식
+
+// .left_on,   .right_on
+
+// .left_on{
+//  left:100px !important;
+// }
+
+// .right_on{
+//  right:100px !important;
+// }
