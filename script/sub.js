0a231c399299c425190c48eb449e35682fe53d28
nicee1009 <je1009@hanmail.net>
2023-03-07 18:20:15 +0900

    sub

--- script/sub.js
+++ script/sub.js
@@ -1,15 +1,19 @@
 $(function(){
 
   $('#down_btn').click(function(){
-    $('html, body').animate({scrollTop:$('.intro').offset().top-70},500,'easeOutQuint');
+    $('html, body').animate({scrollTop:$('.intro').offset().top-0},500,'easeOutQuint');
   });
 
-  //원형내비게이션 클릭시 해당 콘텐츠가 상단으로 올라오게 하기
+  //내비게이션 클릭시 해당 콘텐츠가 상단으로 올라오게 하기
+
   let mnu_n = $('#m_nav li')
 
   mnu_n.click(function(){
     let n = $(this).index();
     console.log(n);
-
+    
+    $('html, body').animate({scrollTop:$('section').eq(n+1).offset().top-0},500);
+    return false;
   });
+
 });
