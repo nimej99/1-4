8f796b2dd420109713dfd19a36f83acedca45314
nicee1009 <je1009@hanmail.net>
2023-03-07 18:58:11 +0900

    sub

--- script/sub.js
+++ script/sub.js
@@ -1,19 +1,31 @@
 $(function(){
 
+  // 스크롤 버튼 클릭시 아래 내용으로 이동
   $('#down_btn').click(function(){
     $('html, body').animate({scrollTop:$('.intro').offset().top-0},500,'easeOutQuint');
   });
 
-  //내비게이션 클릭시 해당 콘텐츠가 상단으로 올라오게 하기
+  //원형내비게이션 클릭시 해당 콘텐츠로 이동
 
   let mnu_n = $('#m_nav li')
 
   mnu_n.click(function(){
     let n = $(this).index();
     console.log(n);
-    
+
     $('html, body').animate({scrollTop:$('section').eq(n+1).offset().top-0},500);
     return false;
   });
 
+  //인트로 텍스트 페이드인
+  $(window).scroll(function(){
+
+        let bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        let bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        if( bottom_of_window > 0 ){
+          $('#main_intro_txt').fadeIn();
+        }
+        
+    }); 
 });
