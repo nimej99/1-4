59597d56919dafe1c75a3967b87f689abc7506ec
nicee1009 <je1009@hanmail.net>
2023-03-09 12:00:29 +0900

    sub

--- script/sub.js
+++ script/sub.js
@@ -78,7 +78,7 @@ $(function(){
     }
 
     // 비전과 미션 박스 날아오게
-    if(sPos>=1870 && sPos<=3200){
+    if(sPos>=1800 && sPos<=3200){
       $('.mission_box').addClass('left_on');
       $('.vision_box').addClass('right_on');
     }else{
@@ -117,6 +117,47 @@ $(function(){
       $("#m_box3").css('display','none');
       });
     }
+  });
+
+  // 모달창 띄우기
+  $('.video_box, .play_btn').click(function(){
+    const modal = `
+    <div class="modal">
+      <img src="../images/sub/close_btn.png" alt="닫기버튼" id="c_btn">
+      <div class="modal_wrap">
+        <div class="left_box">
+        <iframe src="https://www.youtube.com/embed/mJwVPKbKq_A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        <div class="right_box">
+        <h3>하나금융그룹 홍보영상</h3>
+        <p>자산관리의 리더 하나금융그룹<br>
+        자산관리의 리더로서 손님의 소중한 자산을 위해 스스로 이룬 혁신을 다시 넘어서고 손님의 더 큰 만족을 위해 차별화된 기술을 더하는 자산관리의 리더 하나금융그룹
+        <br>
+        <br>
+        글로벌리더 하나금융그룹<br>
+        글로벌 리더로서 세계 속에서 앞서가는 금융이 되기 위해 어제의 자신과 경쟁하고 대한민국 금융의 네트워크를 지구 반대편까지 넓혀가는 글로벌 리더 하나금융그룹
+        <br>
+        <br>
+        상생의 리더 하나금융그룹<br>
+        상생의 리더로서 함께 성장하기 위해 늘 힘이 되어 주고 모두의 행복을 위해 끊임없이 다가가는 상생의 리더 하나금융그룹
+        <br>
+        <br>
+        오늘보다 내일 더 앞서가기 위해 하나금융그룹은 하나금융그룹과 경쟁하고 있습니다.</p>
+        </div>
+      </div>
+    </div>
+    `
+
+    //body태그의 안쪽 맨뒤에 내용을 추가한다.
+    $('body').append(modal);
+    $('body').css('overflow', 'hidden');
+
+    $('.modal:not(.modal_wrap), #c_btn').click(function(){
+      if (!$(event.target).closest('.modal_wrap').length) {
+        $('.modal').hide();
+        $('body').css('overflow','auto');
+      }
+    });
   });
 });
 
