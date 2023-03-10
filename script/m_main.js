020358eef8c50b67975672f4f8268d873a716e80
youjy12 <ppc98@naver.com>
2023-03-10 17:16:56 +0900

    slide
    dd

--- script/m_main.js
+++ script/m_main.js
@@ -1,9 +1,13 @@
 var swiper = new Swiper('.swiper-container', {
   autoplay: {
     delay: 3000, // 3초마다 자동 슬라이드
+    disableOnInteraction: false, // false-스와이프 후 자동 재생
   },
+  loop : true,   // 슬라이드 반복 여부
+  loopAdditionalSlides : 1,
   pagination: {
     el: '.swiper-pagination',
+    clickable: true, // 버튼 클릭 여부
   },
   navigation: {
     nextEl: '.swiper-button-next',
