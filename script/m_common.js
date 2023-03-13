95b99e3ba753f314009e3723f0f227edacb45c6f
nimej9909 <aaron9909@naver.com>
2023-03-13 12:08:40 +0900

    common

--- script/m_common.js
+++ script/m_common.js
@@ -26,3 +26,14 @@ mGnb.addEventListener('change', function(){
       document.querySelector('body').style.overflowY = '';
   }
 });
+
+// 피시버전 돌아가기
+const isPc = window.matchMedia("only screen and (min-width: 767px)").matches;
+if (isPc) {
+    let href = document.location.href;
+    let cut_href = href.split('/');
+    let cut2 = cut_href[4];
+    let cut3 = cut2.split('_');
+        console.log(cut3[1]);
+        location.href='http://youjy12.dothome.co.kr/1-4/'+cut3[1];
+    }
