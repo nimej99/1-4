5cef5715409fd952439085682bce8151ce428fa9
nimej9909 <aaron9909@naver.com>
2023-03-14 20:42:01 +0900

    common

--- script/m_common.js
+++ script/m_common.js
@@ -53,15 +53,15 @@ mGnb.addEventListener('change', function(){
         }
 
 // 피시버전 돌아가기
-// const isPc = window.matchMedia("only screen and (min-width: 767px)").matches;
-// if (isPc) {
-//     let hRef = document.location.href;
-//     let cut_href = hRef.split('/');
-//     let cut2 = cut_href[4];
-//     let cut3 = cut2.split('_');
-//         console.log(cut3[1]);
-//         location.href='http://youjy12.dothome.co.kr/1-4/'+cut3[1];
-//     }
+const isPc = window.matchMedia("only screen and (min-width: 767px)").matches;
+if (isPc) {
+    let hRef = document.location.href;
+    let cut_href = hRef.split('/');
+    let cut2 = cut_href[4];
+    let cut3 = cut2.split('_');
+        console.log(cut3[1]);
+        // location.href='http://youjy12.dothome.co.kr/1-4/'+cut3[1];
+    }
 
 //100vh 버그 픽스
 
